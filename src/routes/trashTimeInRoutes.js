7f5adc7a98b1dce9d33d944098b95b543542fbdf
Weslley de Campos <weslleydrum@hotmail.com>
2020-03-25 09:18:35 -0300

    aaaa

---
+++ src/routes/trashTimeInRoutes.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const TrashTimeIn = require('../controllers/TrashTimeIn');
+const TrashTimeInRoutes = express.Router();
+const authmiddlaware = require('../middlewares/auth')
+
+TrashTimeInRoutes.post()
